af36a00a34b4bac32afaa1986a25076071bd5f90
PieroTandaypanValle <115677852+PieroTandaypanValle@users.noreply.github.com>
2022-12-13 22:40:45 -0300

    Relacionando las base de datos

--- web/posts/models.py
+++ web/posts/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts', null=True)
     #image = models.ImageField()
     likes = models.PositiveIntegerField(default=0, blank=True)
     create_at = models.DateTimeField(auto_now_add=True)
     quote=models.TextField(null=True, blank=True)
 
-class Comment(models.Model): 
+class Comment(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments', null=True)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments", null=True)
     text = models.TextField()   
