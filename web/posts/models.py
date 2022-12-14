3f2035262d5ed7379cd8b713820e7c0dc3b00323
PieroTandaypanValle <115677852+PieroTandaypanValle@users.noreply.github.com>
2022-12-13 22:25:33 -0300

    Creando modelos, base de datos y registro

--- web/posts/models.py
+++ web/posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    #image = models.ImageField()
+    likes = models.PositiveIntegerField(default=0, blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    quote=models.TextField(null=True, blank=True)
+
+class Comment(models.Model): 
+    text = models.TextField()   
